fac3de1aca186ec87425a0e49a431896107b6e0e
Kanav Dhir <kdhir@bu.edu>
2014-10-19 12:23:23 -0700

    Setup class for chess board cells

---
+++ Lab4_KD/Lab4_KD/Cell.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab4_KD
+{
+    class Cell
+    {
+        // Row and Column values for each Cell
+        public int row;
+        public int col;
+
+        // Cell Color
+        public Brush color;
+
+        // Flag for wheather the cell is safe or not (default true)
+        public bool safe = true;
+
+        // Rect to represent each cell (x,y,width,height)
+        public Rectangle rect;
+
+        //Default Constructor
+        public Cell(Rectangle currRect, int currRow, int currCol, Brush currColor)
+        {
+            // Set cell's rect
+            this.rect = currRect;
+
+            // Set cell color
+            this.color = currColor;
+
+            // Set row and column
+            this.row = currRow;
+            this.col = currCol;
+            
+        }
+    }
+}
